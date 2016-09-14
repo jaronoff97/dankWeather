a9dc7f67e24d9243a9496b5c4a66b34a5abd67d1
mattrberry <mattrossberry@gmail.com>
2016-09-13 20:40:47 -0400

    Made weather danker.

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from flask import Flask
 from math import log
 import requests
 import json
+import os
 
 app = Flask(__name__)
+forecastAPIKey = os.environ.get('FORECAST_IO_API_KEY')
+
 
 @app.route('/')
 def home():
-    response = requests.get("https://api.forecast.io/forecast/50d386c2f9677bf46394c4a2180a606f/42.360082,-71.058880")
+    response = requests.get(
+        "https://api.forecast.io/forecast/{0}/42.360082,-71.058880".format(forecastAPIKey))
     data = json.loads(response.text)
     latitude = data["latitude"]
     longitude = data["longitude"]
@@ -16,7 +20,8 @@ def home():
     wind = data["currently"]["windSpeed"]
     rain = data["currently"]["precipIntensity"]
 
-    return str(round(dankCalc(temp, humid, wind, rain), 5)) + "% dank"
+    return str(int(dankCalc(temp, humid, wind, rain))) + "%"
+
 
 def dankCalc(temp, humid, wind, rain):
     temp = dankTemp(temp)
@@ -26,17 +31,20 @@ def dankCalc(temp, humid, wind, rain):
 
     return (temp + humid + wind + rain) / 4
 
+
 def dankTemp(temp):
     if temp > 40 or temp <= 100:
         return (100 - abs(70 - temp) ** 1.354)
     else:
         return 0
 
+
 def dankHumid(humid):
     if humid < .5:
-        return (100 - 100 * abs(.25 - humid) ** ((log(2,10) + log(5,10)) / log(5,10)))
+        return (100 - 100 * abs(.25 - humid) ** ((log(2, 10) + log(5, 10)) / log(5, 10)))
     else:
         return 0
+
 
 def dankWind(wind):
     if wind < 26.5:
@@ -44,8 +52,9 @@ def dankWind(wind):
     else:
         return 0
 
+
 def dankRain(rain):
-    if rain < .1: 
+    if rain < .1:
         return (100 - rain * 1000)
     else:
         return 0
